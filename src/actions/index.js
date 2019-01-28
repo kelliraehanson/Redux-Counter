61ffc688f5c722ebb04139fbe29ea46aded20c78
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-01-28 14:39:06 -0600

    commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,7 @@ export const DECREMENT = 'DECREMENT';
 // Note that the action creator is not at all
 // responsible for handling any of the actual
 // logic of updating the central Redux store. That
-// is left to the reducer(s).
+// is left to the reducer(s). hi
 
 export const increment = () => {
   // Fill in this function
